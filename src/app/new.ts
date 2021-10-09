e712379d1b32f518707cb7a1246fe772ed636b70
NastiaKoltyka <Nusia.k5669@gmail.com>
2021-10-09 19:53:48 +0300

    add news and profile pages

---
+++ src/app/new.ts
@@ -0,0 +1,14 @@
+export class New {
+    title: string;
+    content: string;
+    url: string;
+    urlToImage: string;
+
+
+    constructor(title: string, content: string, url: string, urlToImage: string) {
+        this.title='';
+        this.content='';
+        this.url='';
+        this.urlToImage='';
+    }
+}
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { New } from '../new';
 
 @Component({
   selector: 'app-news',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
+news:New[]=[]
+  constructor() {
+    // this.news='';
+    fetch('https://newsapi.org/v2/everything?q=Apple&from=2021-10-09&sortBy=popularity&apiKey=c5f654ce1b3b4c7b84448955484fbb73')
+      .then(response => response.json())
+      .then( (data) => {
+        this.news=data.articles
+        console.log(this.news)
+      })
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
